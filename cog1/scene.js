b9da141360324bc710c807830fd98c1161b074ce
crisi3 <christine.kuczera@data4life.care>
2021-10-02 01:22:31 +0200

    Updates

--- cog1/scene.js
+++ cog1/scene.js
@@ -381,6 +381,9 @@ function(exports, dojo, domStyle, app, scenegraph, createScene, animation, raste
 
 		// Loop over polygons in model.
 		for(var p = 0; p < polygons.length; p++) {
+			// Polygon counter
+			polycount ++;
+
 			// Prepare the data of polygon p to pass to scanline.
 			var polygon = polygons[p];
 			var normal = polygonNormals[p];
